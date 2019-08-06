2f5803fe475d703ee123db08647f03175f180cd8
mark <markgromano31@gmail.com>
2019-08-05 22:16:23 -0400

    ternsberg for routing lol;

---
+++ src/components/Login/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './Login.css'
+
+export default ({ userSession }) => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <button onClick={() => userSession.redirectToSignIn()}>Log in!!!</button>
+      </header>
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom'
 
 import './index.css';
+
 import App from './components/App';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <App />,
   document.getElementById('root')
 );
