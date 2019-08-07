87f3e83663b781ddece1cc66fa5d6e0d254a9c74
erikwlarsen <erik.w.larsen@gmail.com>
2019-08-07 01:02:15 -0400

    feat: world's ugliest autocomplete

---
+++ src/components/SelectUser/index.js
@@ -0,0 +1,52 @@
+import { User } from 'radiks';
+import React, { useState, useEffect } from 'react';
+import Input from '@material-ui/core/Input';
+import MenuItem from '@material-ui/core/MenuItem';
+
+export default () => {
+  const [search, setSearch] = useState('');
+  const [userList, setUserList] = useState([]);
+  const [selectedUsers, setSelectedUsers] = useState([]);
+
+  useEffect(() => {
+    if (search.length > 2) {
+      User.fetchList({ username: { $regex: search } })
+        .then(userList => {
+          setUserList(userList.map(user => user.attrs.username).slice(0, 10));
+        });
+    } else {
+      setUserList([]);
+    }
+  }, [search]);
+
+  const selectUser = (user) => {
+    setSelectedUsers(selectedUsers.concat(user));
+  };
+
+  const deselectUser = (user) => {
+    const userIndex = selectedUsers.indexOf(user);
+    setSelectedUsers(selectedUsers.slice(0, userIndex).concat(selectedUsers.slice(userIndex + 1)));
+  };
+
+  return (
+    <div>
+      <Input value={search} onChange={evt => setSearch(evt.target.value)}/>
+      {selectedUsers.length ? selectedUsers.map(selected => (
+        <MenuItem
+          key={selected}
+          value={selected}
+          onClick={() => deselectUser(selected)}
+        >Selected: {selected}</MenuItem>
+      )) : null}
+      {userList.length ? userList
+        .filter(user => !selectedUsers.includes(user))
+        .map((user, index) =>
+          <MenuItem
+            key={user}
+            value={user}
+            onClick={() => selectUser(user)}
+          >{user}</MenuItem>)
+      : null}
+    </div>
+  );
+}
--- src/components/UploadFile/index.js
+++ src/components/UploadFile/index.js
@@ -5,6 +5,7 @@ import Fab from '@material-ui/core/Fab';
 import AddIcon from '@material-ui/icons/Add';
 import { makeStyles } from '@material-ui/core/styles';
 import { User, getConfig } from 'radiks';
+import SelectUser from '../SelectUser';
 
 const useStyles = makeStyles(theme => ({
     fab: {
@@ -28,24 +29,27 @@ const UploadFile = () => {
   handleSignIn();
   const classes = useStyles();
   return (
-   <div className="share-file-container">
-    <section className="share-file-main">
-      <div className="share-file-upload-file">
-        <Fab color="primary" aria-label="add" className={classes.fab}>
-          <AddIcon />
-        </Fab>
-        <p className="no-margin">Drag and drop files</p>
-        <p className="no-margin"> or click to send up to X FILE SIZE</p>
-        <Button variant="contained" color="primary" className={classes.button}>
-          Upload Shiz
-        </Button>
-      </div>
-      <div className="share-file-description">
-        <h1 className="no-margin"> Simple Private BlockSnack File Sharing</h1>
-        <p>Juicy meatballs brisket slammin' baked shoulder. Juicy smoker soy sauce burgers brisket. polenta mustard hunk greens. Wine technique snack skewers chuck excess. Oil heat slowly.</p>
-      </div>
-    </section>
-   </div>
+    <div>
+      <div className="share-file-container">
+        <section className="share-file-main">
+        <div className="share-file-upload-file">
+          <Fab color="primary" aria-label="add" className={classes.fab}>
+            <AddIcon />
+          </Fab>
+          <p className="no-margin">Drag and drop files</p>
+          <p className="no-margin"> or click to send up to X FILE SIZE</p>
+          <Button variant="contained" color="primary" className={classes.button}>
+            Upload Shiz
+          </Button>
+        </div>
+        <div className="share-file-description">
+          <h1 className="no-margin"> Simple Private BlockSnack File Sharing</h1>
+          <p>Juicy meatballs brisket slammin' baked shoulder. Juicy smoker soy sauce burgers brisket. polenta mustard hunk greens. Wine technique snack skewers chuck excess. Oil heat slowly.</p>
+        </div>
+      </section>
+    </div>
+    <SelectUser />
+  </div>
   );
 }
 
