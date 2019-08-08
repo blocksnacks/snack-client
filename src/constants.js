53a73504706b7c6d2d63fdab0774780eb59c5269
mark <markgromano31@gmail.com>
2019-08-08 00:57:14 -0400

    rm c.log

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,3 @@
 export const USER_SETTINGS = 'UserSettings';
 export const ERROR_POSTING_EMAILS = 'ErrorPostingEmails';
-const RADIKS_URL = process.env.RADIKS || 'http://localhost:1260';
+export const RADIKS_URL = process.env.RADIKS || 'http://localhost:1260';
