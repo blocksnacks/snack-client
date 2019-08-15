a920f020128dd54662e521bc8ff412119278fddc
Erik Larsen <erik.w.larsen@gmail.com>
2019-08-14 22:33:58 -0400

    feat: add no username message

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -2,11 +2,12 @@ import React from 'react'
 
 import './Login.css'
 
-export default ({ userSession }) => {
+export default ({ userSession, nullUsername }) => {
   return (
     <div className="App">
       <header className="App-header">
         <button onClick={() => userSession.redirectToSignIn()}>Log in!!!</button>
+        {nullUsername && <p>Your username is not yet registered. Please come back again once your registration process is complete.</p>}
       </header>
     </div>
   );
