cd8ec8f153f794433e97b24078e25f08f400d8ac
Mary Snow <marysnow@Marys-MacBook-Pro.local>
2019-08-22 21:31:26 -0400

    small styling shit

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import {Button } from '@material-ui/core';
 
 import './Login.css'
 
@@ -6,8 +7,16 @@ export default ({ userSession, nullUsername }) => {
   return (
     <div className="App">
       <header className="App-header">
-        <button onClick={() => userSession.redirectToSignIn()}>Log in!!!</button>
-        {nullUsername && <p>Your username is not yet registered. Please come back again once your registration process is complete.</p>}
+        <div>
+          <Button 
+          onClick={() => userSession.redirectToSignIn()}
+          variant="contained"
+          color="primary"
+          >
+            Log in
+          </Button>
+          {nullUsername && <p>Your username is not yet registered. Please come back again once your registration process is complete.</p>}
+        </div>
       </header>
     </div>
   );
