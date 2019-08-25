e38ca9b491342b3e63bfee188f7a4d6b88852633
mark <markgromano31@gmail.com>
2019-08-25 19:13:44 -0400

    display table of sharedys

--- src/components/Shared/index.js
+++ src/components/Shared/index.js
@@ -7,21 +7,35 @@ import {
   TableCell,
   TableBody
 } from '@material-ui/core';
+import bytes from 'bytes';
+import './Shared.css';
 
 import CircularProgress from '../CircularProgress';
 
 import './Shared.css';
 import { authNeeded, addNavbar } from '../hocs';
 
+import SharedDocument from '../../models/SharedDocument';
+
 const Shared = () => {
-  const [groups, setGroups] = useState([]);
+  const [docs, setDocs] = useState([]);
   const [errorFetching, setErrorFetching] = useState(false);
 
   useEffect(() => {
     (async function () {
       try {
         const groups = await UserGroup.myGroups();
-        setGroups(groups.map(({ attrs }) => ({ id: attrs._id, name: attrs.name })));
+        const docs = (await SharedDocument.fetchList({ userGroupId: groups[0].id }))
+          .filter(({ attrs }) => typeof attrs.name === 'string')
+          .map(({ attrs}) => ({
+            id: attrs._id,
+            name: attrs.name,
+            createdDate: attrs.createdAt,
+            size: attrs.size,
+            uploadedBy: attrs.uploadedBy,
+            content: attrs.content,
+          }))
+        setDocs(docs);
       } catch (err) {
         console.error(err);
         setErrorFetching(true)
@@ -30,26 +44,34 @@ const Shared = () => {
   }, [])
 
   return (
-    <div className="shared-container">
-      <h2>
+    <div>
+      <h2 className="table-title">
         Files Shared with me
       </h2>
       {errorFetching
         ? <div>Error retrieving shared files</div>
-        : !groups.length
+        : !docs.length
           ? <CircularProgress />
           : (
             <div className="table-container">
               <Table>
                 <TableHead>
-                  <TableCell>File Name</TableCell>
                   <TableCell>File ID</TableCell>
+                  <TableCell>File Name</TableCell>
+                  <TableCell>Created At</TableCell>
+                  <TableCell>Size</TableCell>
+                  <TableCell>Uploaded By</TableCell>
+                  <TableCell>Download</TableCell>
                 </TableHead>
                 <TableBody>
-                  {groups.map(({ id, name }) => (
+                  {docs.map((doc) => (
                     <TableRow>
-                      <TableCell>{name}</TableCell>
-                      <TableCell>{id}</TableCell>
+                      <TableCell>{doc.id}</TableCell>
+                      <TableCell>{doc.name}</TableCell>
+                      <TableCell>{doc.createdDate}</TableCell>
+                      <TableCell>{bytes(doc.size)}</TableCell>
+                      <TableCell>{doc.uploadedBy}</TableCell>
+                      <TableCell>DL</TableCell>
                     </TableRow>
                   ))}
                 </TableBody>
